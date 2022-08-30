d2feb1f214de1dd3e273d3775c4b734892cfa210
Conner Drake <conner@boardlovers.com>
2022-08-30 12:01:54 -0400

    Responsiveness - small desktop viewports

--- src/ButtonContainers.js
+++ src/ButtonContainers.js
@@ -1,22 +1,22 @@
 // structure button info into an object for each button and also add the button keys here so they can be added when the buttons are rendered
 
-const DEFAULT_BUTTON_HEIGHT = "6.5vh";
-const DEFAULT_NUM_BUTTON_WIDTH = "7vh";
-const DEFAULT_OP_BUTTON_WIDTH = "7vh";
+const DEFAULT_BUTTON_HEIGHT = "20%";
+const DEFAULT_NUM_BUTTON_WIDTH = "27%";
+const DEFAULT_OP_BUTTON_WIDTH = "37%";
 
 export const BUTTON_CONTAINERS = {
   id: "root-button-container",
   style: {
-    height: "40vh",
-    width: "75vh",
+    height: "70%",
+    width: "99%",
   },
   classes: "center flex",
   children: [
     {
       id: "numeric",
       style: {
-        height: "37vh",
-        width: "27vh",
+        height: "93%",
+        width: "53%",
       },
       classes: "center flex padded-container",
       buttons: [
@@ -102,8 +102,8 @@ export const BUTTON_CONTAINERS = {
     {
       id: "auxilliaries",
       style: {
-        height: "37vh",
-        width: "18vh",
+        height: "92%",
+        width: "38%",
       },
       classes: "center flex padded-container",
 
